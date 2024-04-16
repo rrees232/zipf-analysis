4d0353e4bd1d66379a458b80a2d0d7874f0e5644
Rebecca Rees <rebeccarees@Rebeccas-MacBook-Pro.local>
2024-04-16 13:40:49 +0100

    automate analysis

---
+++ bin/run_analysis.sh
@@ -0,0 +1,6 @@
+mkdir -p results
+for book in dracula frankenstein
+do
+    python bin/countwords.py data/${book}.txt --num 100 > results/${book}.csv
+    python bin/plotcounts.py results/${book}.csv --outfile results/${book}.pdf
+done
